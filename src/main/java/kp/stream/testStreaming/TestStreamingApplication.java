76e3ce1c6325c3195cff5f0359257bb6e584f473
krunal <krunalhp123@gmail.com>
2020-04-24 22:41:15 +0530

    Sample code commit

--- src/main/java/kp/stream/testStreaming/TestStreamingApplication.java
+++ src/main/java/kp/stream/testStreaming/TestStreamingApplication.java
@@ -2,13 +2,16 @@ package kp.stream.testStreaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TestStreamingApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(TestStreamingApplication.class, args);
+		SpringApplication.
+				run(TestStreamingApplication.class, args);
 	}
 
 }
