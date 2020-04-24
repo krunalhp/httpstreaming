6bed37b047381bcc6c4c2a5c92831831311ac450
krunal <krunalhp123@gmail.com>
2020-04-24 22:34:04 +0530

    Initial setup

---
+++ src/main/java/kp/stream/testStreaming/TestStreamingApplication.java
@@ -0,0 +1,14 @@
+package kp.stream.testStreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStreamingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TestStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kp/stream/testStreaming/TestStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package kp.stream.testStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
